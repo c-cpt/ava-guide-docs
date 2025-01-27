7e955723198fde828ad14a542c98458914df41be
Chad Compton <chad@flywithhive.com>
2025-01-27 15:11:37 -0700

    SAtheme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+// @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
@@ -6,19 +7,23 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [
     starlight({
-      title: "Developer Series",
+      title: "",
       social: {
         github: "https://github.com/ava-labs",
       },
       editLink: {
         baseUrl: "https://github.com/c-cpt/ava-guide-docs/edit/main/",
       },
+      logo: {
+        src: "./src/assets/dev_logo.svg",
+        replacesTitle: true,
+      },
       sidebar: [
         {
           label: "Guides",
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: "9000 Guide", slug: "guides/avalanche9000" },
+            { label: "Example Guide", slug: "guides/example" },
           ],
         },
         {
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,40 @@
-import colors from 'tailwindcss/colors';
-import starlightPlugin from '@astrojs/starlight-tailwind';
+// import colors from "tailwindcss/colors";
+import starlightPlugin from "@astrojs/starlight-tailwind";
+
+// Generated color palettes
+const accent = {
+  200: "#FF394A",
+  600: "#a40021",
+  900: "#600c14",
+  950: "#421012",
+};
+const gray = {
+  100: "#f6f6f7",
+  200: "#ededef",
+  300: "#c2c2c4",
+  400: "#8b8b8f",
+  500: "#58585b",
+  700: "#38383b",
+  800: "#262629",
+  900: "#181819",
+};
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
-				accent: colors.indigo,
-				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-				gray: colors.zinc,
-			},
-		},
-	},
-	plugins: [starlightPlugin()],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  //   theme: {
+  //     extend: {
+  //       colors: {
+  //         // Your preferred accent color. Indigo is closest to Starlight’s defaults.
+  //         accent: accent,
+  //         // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
+  //         // gray: colors.zinc,
+  //         gray: gray,
+  //       },
+  //     },
+  //   },
+  theme: {
+    colorScheme: "dark", // Set the default color scheme to 'dark'
+  },
+  plugins: [starlightPlugin()],
 };
