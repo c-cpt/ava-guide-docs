185cce47bdc54793821c3df2a04895b00fc76c63
Chad Compton <chad@flywithhive.com>
2025-02-22 07:32:22 -0700

    layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,52 +3,51 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      head: [
-        // Example: add Fathom analytics script tag.
-        {
-          tag: "script",
-          attrs: {
-            src: "/plugins/theme.js",
+  integrations: [starlight({
+    head: [
+      // Example: add Fathom analytics script tag.
+      {
+        tag: "script",
+        attrs: {
+          src: "/plugins/theme.js",
+        },
+      },
+    ],
+    favicon: "/favicon.ico",
+    title: "",
+    social: {
+      github: "https://github.com/ava-labs",
+    },
+    editLink: {
+      baseUrl: "https://github.com/c-cpt/ava-guide-docs/edit/main/",
+    },
+    logo: {
+      src: "./src/assets/dev_logo.svg",
+      replacesTitle: true,
+    },
+    components: {
+      Hero: './src/components/Hero.astro',
+    },
+    sidebar: [
+      {
+        label: "Guides",
+        items: [
+          // Each item here is one entry in the navigation menu.
+          {
+            label: "Getting Started With Blockchain Development",
+            slug: "guides/getting_started_with_blockchain_development",
           },
-        },
-      ],
-      favicon: "/favicon.ico",
-      title: "",
-      social: {
-        github: "https://github.com/ava-labs",
+        ],
       },
-      editLink: {
-        baseUrl: "https://github.com/c-cpt/ava-guide-docs/edit/main/",
+      {
+        label: "Reference",
+        autogenerate: { directory: "reference" },
       },
-      logo: {
-        src: "./src/assets/dev_logo.svg",
-        replacesTitle: true,
-      },
-      components: {
-        Hero: './src/components/Hero.astro',
-      },
-      sidebar: [
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            {
-              label: "Getting Started With Blockchain Development",
-              slug: "guides/getting_started_with_blockchain_development",
-            },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
-      customCss: ["./src/tailwind.css"],
-    }),
-    tailwind({ applyBaseStyles: false }),
-  ],
+    ],
+    customCss: ["./src/tailwind.css"],
+  }), tailwind({ applyBaseStyles: false }), react()],
 });
