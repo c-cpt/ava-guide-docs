62d4eef26d07d39a413a387a1a95ce62bc2ab7ae
Chad Compton <chad@flywithhive.com>
2025-02-24 08:11:12 -0700

    mv to assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    ssr: {
-      noExternal: ["hast-util-to-html", "zwitch"],
-    },
-  },
   integrations: [
     starlight({
       head: [
