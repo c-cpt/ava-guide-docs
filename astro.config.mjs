d7e7476286b2b3abda787ddd5bb9f9fb93e4d5e5
Chad Compton <chad@flywithhive.com>
2025-02-24 08:45:30 -0700

    noExternal

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    ssr: {
+      noExternal: ["hast-util-to-html", "zwitch"],
+    },
+  },
   integrations: [
     starlight({
       head: [
